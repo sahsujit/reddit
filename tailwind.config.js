e9ce75bee4f521722ec0a1b3378fda6181c5adef
sujit sah <sujit8877123sah@gmail.com>
2023-09-30 14:21:28 +0545

    Remove node_modules from tracking

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        mulish:["mulish", "sans-serif"]
+      },
+      colors:{
+        secondary:"#217.2 32.6% 17.5%",
+
+      },
+    },
+  },
+  plugins: [],
+}
+
